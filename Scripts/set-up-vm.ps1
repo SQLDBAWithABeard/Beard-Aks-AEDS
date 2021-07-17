05df17a73bac1f4f58c81ae6b4d95df83045f6d4
SQLDbaWithABeard <mrrobsewell@outlook.com>
2021-07-17 14:24:28 +0100

    not needed and no prompt

--- Scripts/set-up-vm.ps1
+++ Scripts/set-up-vm.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install azure-data-studio -y
-choco install pwsh
+choco install pwsh -y
 choco install vscode -y 
 choco install vscode-powershell
 choco install azure-cli -y 
@@ -9,7 +9,11 @@ choco install kubernetes-cli -y
 choco install kubernetes-helm -y
 choco install git -y
 
+<#
 $File = "$ENV:temp\azdata.msi"
 Invoke-WebRequest -Uri https://aka.ms/azdata-msi -OutFile $file
 msiexec  /i $file
+
+#>
+
 
