abac6ee250f8afeba5b6413846c37c086022f9fd
SQLDbaWithABeard <mrrobsewell@outlook.com>
2021-07-30 09:01:50 +0100

    additional cron job for fun

--- Scripts/localk8s/deploycronjobs.ps1
+++ Scripts/localk8s/deploycronjobs.ps1
@@ -1,9 +1,11 @@
 kubectl delete -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellfailedlogin.yaml
 kubectl delete -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellrunascript.yaml
+kubectl delete -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellrunascript-ben.yaml
 
 
 kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellfailedlogin.yaml
 kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellrunascript.yaml
+kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellrunascript-ben.yaml
 
 
 
