387e404d65e78ab12d7c3ab23054bbdc55a7d3ae
SQLDbaWithABeard <mrrobsewell@outlook.com>
2021-08-04 13:47:38 +0100

    new jobs

--- Scripts/localk8s/deploycronjobs.ps1
+++ Scripts/localk8s/deploycronjobs.ps1
@@ -4,8 +4,9 @@ kubectl delete -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\p
 
 
 kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellfailedlogin.yaml
+kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellfailedlogin-aks.yaml
 kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellrunascript.yaml
-kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellrunascript-ben.yaml
+kubectl apply -f G:\OneDrive\Documents\GitHub\Beard-Aks-AEDS\Scripts\localk8s\powershellrunascript-ben-aks.yaml
 
 
 
